09b461d3c7fc177b69eaa295d17677a5158edd77
sejalsuryavanshi <75065798+sejalsuryavanshi@users.noreply.github.com>
2020-12-31 14:08:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function draw() {
   
   if(wall.x-car.x<(car.width+wall.width)/2){
     car.velocityX=0;
-    var deformation=0.5*weigth*speed*speed/22509;
+    var deformation=0.5*weight*speed*speed/22509;
     if(deformation>180){
       car.shapeColor=color("yellow");
       wall.shapeColor=color("white");
